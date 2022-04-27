48b5e1173f5f70ada07986ff63fd632e1cd0f7a8
Lintao Fan <lintaofan@gmail.com>
2022-04-26 23:11:05 -0700

    refactoring configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,43 @@
 import { Button, Container, Flex, TabList, Tabs, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
 import { create } from 'domain';
-import { useState, useContext, createContext } from 'react';
+import { useState, useContext, createContext, useReducer } from 'react';
 import { api, IQuestion } from '../src/api/api';
-import { Configuration } from '../src/section/configuration';
+import { Configuration, reduceSelectedItems } from '../src/section/configuration';
 import Question from '../src/section/question';
 import { Yuanwen } from '../src/section/yuanwen';
 
-var any:any = null;
+var any: any = null;
 export const AppContext = createContext(any);
 const IndexPage = () => {
   const [trys, setTrys] = useState([])
   const [fenjus, setFenjus] = useState([])
   const [answers, setAnswers] = useState([])
-  const [checkedItems, setCheckedItems] = useState(["3分句"])
   const [yuanwen, setYuanwen] = useState("")
-  
+  const [checkedItems, checkItems] = useReducer(reduceSelectedItems, ["3分句"])
+
 
   return (
-    <AppContext.Provider value={{checkedItems, setCheckedItems,trys, setTrys, fenjus, setFenjus, answers, setAnswers, yuanwen,setYuanwen}}>
-    <Container maxW="container.xl" p={0}>
-      <Tabs>
-        <TabList>
-          <Tab>Question</Tab>
-          <Tab>Configuration</Tab>
-          <Tab>Yuanwen</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
+    <AppContext.Provider value={{ checkedItems, checkItems, trys, setTrys, fenjus, setFenjus, answers, setAnswers, yuanwen, setYuanwen }}>
+      <Container maxW="container.xl" p={0}>
+        <Tabs>
+          <TabList>
+            <Tab>Question</Tab>
+            <Tab>Configuration</Tab>
+            <Tab>Yuanwen</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
               <Question />
-          </TabPanel>
-          <TabPanel>
-            <Configuration />
-          </TabPanel>
-          <TabPanel>
-            <Yuanwen />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Container>
+            </TabPanel>
+            <TabPanel>
+              <Configuration />
+            </TabPanel>
+            <TabPanel>
+              <Yuanwen />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Container>
     </AppContext.Provider>
   )
 };
--- src/section/configuration.tsx
+++ src/section/configuration.tsx
@@ -1,48 +1,43 @@
 import { Checkbox, HStack } from '@chakra-ui/react';
+import { startCase } from 'lodash';
 import { useContext } from 'react';
 import { AppContext } from '../../pages';
 
-export function Configuration() {
-  const { checkedItems, setCheckedItems } = useContext(AppContext);
-  function check(checked: boolean, fenju: string) {
-    if (checked) {
-      checkedItems.push(fenju);
-      console.log('add' + fenju);
-      console.log('added' + checkedItems);
-      setCheckedItems([...checkedItems]);
-    } else {
-      const index = checkedItems.indexOf(fenju);
-      if (index !== -1) {
-        checkedItems.splice(index, 1);
-      }
-      console.log('filter' + fenju);
-      console.log('filtered ' + checkedItems);
-      setCheckedItems([...checkedItems]);
-    }
+export const reduceSelectedItems = (state: string[], action: { type: boolean, item: string }) => {
+  console.log("reduceSelectedItems", state, action)
+  switch (action.type) {
+    case true:
+      return [...state, action.item];
+    case false:
+      return state.filter(it => it != action.item);
   }
+}
+export function Configuration() {
+  const { checkedItems, checkItems } = useContext(AppContext);
+
   return (
     <HStack pl={6} mt={1} spacing={1}>
       <Checkbox
         isChecked={checkedItems.includes('2分句')}
-        onChange={(e) => check(e.target.checked, '2分句')}
+        onChange={(e) => checkItems({ type: e.target.checked, item: '2分句' })}
       >
         2分句
       </Checkbox>
       <Checkbox
         isChecked={checkedItems.includes('3分句')}
-        onChange={(e) => check(e.target.checked, '3分句')}
+        onChange={(e) => checkItems({ type: e.target.checked, item: '3分句' })}
       >
         3分句
       </Checkbox>
       <Checkbox
         isChecked={checkedItems.includes('4分句')}
-        onChange={(e) => check(e.target.checked, '4分句')}
+        onChange={(e) => checkItems({ type: e.target.checked, item: '4分句' })}
       >
         4分句
       </Checkbox>
       <Checkbox
         isChecked={checkedItems.includes('5分句')}
-        onChange={(e) => check(e.target.checked, '5分句')}
+        onChange={(e) => checkItems({ type: e.target.checked, item: '5分句' })}
       >
         5+分句
       </Checkbox>
