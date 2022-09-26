f21e493751bdcf78ed9facc50476342b86e69616
Lintao Fan <lintaofan@gmail.com>
2022-09-25 18:42:47 -0700

    Deploy Next.js to gh-pages

--- _next/static/uqvbTto9TV2FlQXHOf53w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76-cf94112156509fcb.js","static/chunks/pages/index-00611edaf2ef3a9a.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
